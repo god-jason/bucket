4d6dfa087df62f264fd449b58b5b9f44ca468082
jason <jason@zgwit.com>
2024-07-01 18:56:43 +0800

    添加broker

---
+++ broker/hook.go
@@ -0,0 +1,37 @@
+package broker
+
+import (
+	"bytes"
+	"github.com/mochi-mqtt/server/v2"
+	"github.com/mochi-mqtt/server/v2/packets"
+)
+
+type Hook struct {
+	mqtt.HookBase
+}
+
+func (h *Hook) ID() string {
+	return "broker"
+}
+func (h *Hook) Provides(b byte) bool {
+	//高效吗？
+	return bytes.Contains([]byte{
+		mqtt.OnConnectAuthenticate,
+		mqtt.OnACLCheck,
+		mqtt.OnDisconnect,
+	}, []byte{b})
+}
+
+func (h *Hook) OnConnectAuthenticate(cl *mqtt.Client, pk packets.Packet) bool {
+
+	return true
+}
+
+func (h *Hook) OnACLCheck(cl *mqtt.Client, topic string, write bool) bool {
+	//只允许发送属性事件
+	return true
+}
+
+func (h *Hook) OnDisconnect(cl *mqtt.Client, err error, expire bool) {
+
+}
